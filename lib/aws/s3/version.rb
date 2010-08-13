d1af24888bd42f28de78899d9f66852ec5812ead
Gabe da Silveira <gabe@websaviour.com>
2010-08-13 00:31:31 -0700

    Bump to version mubi4

--- lib/aws/s3/version.rb
+++ lib/aws/s3/version.rb
@@ -4,7 +4,7 @@ module AWS
       MAJOR    = '0'
       MINOR    = '6'
       TINY     = '2'
-      BETA     = 'mubi3'
+      BETA     = 'mubi4'
     end
     
     Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY, VERSION::BETA].compact * '.'
